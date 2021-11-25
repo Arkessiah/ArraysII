4cf2eecbbdd511429de7c3ce0013a27a3061e5d7
Arkessiah <amarotoenriquez@gmail.com>
2021-11-25 18:26:55 +0000

    finish

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Crea una variable llamada numeros que sea un array vacío
-
+var numeros = [];
 
 // Crea un bucle for que recorra los números empezando en 0 y llegando hasta 10 (INCLUYENDO EL 10). En cada iteración, empuja ese número i dentro del array numeros
-
+for (var i = 0; i<=8; i++){
+  numeros.push(i);
+}
 
 // Para terminar, vamos a quitar los dos últimos números del array.
