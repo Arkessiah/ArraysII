a7b21767f4235e02270189cceaa0c8bfed26da0b
Arkessiah <amarotoenriquez@gmail.com>
2021-11-26 21:08:24 +0000

    ready

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 var numeros = [];
 
 // Crea un bucle for que recorra los números empezando en 0 y llegando hasta 10 (INCLUYENDO EL 10). En cada iteración, empuja ese número i dentro del array numeros
-for (var i = 0; i<=8; i++){
+for (var i = 0; i<11; i++){
   numeros.push(i);
 }
 
 // Para terminar, vamos a quitar los dos últimos números del array.
+for (var j =0;j <2;j++){
+  numeros.pop();
+}
